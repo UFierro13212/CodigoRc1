20f7f09702f5cf91bee90f22746d15edc4636ebf
UFierro13212 <uriel.fierro@udla.edu.ec>
2026-05-25 10:34:42 -0500

    Declaro Variables

--- Codigo.c
+++ Codigo.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 
 int main() {
+
+    int cantidadPlatos;
+    int i;
+
+    char nombres[5][30];
+    int cantidad[5];
+    float tiempoPorPlato[5];
+    int ingredientesPorPlato[5];
+
+    float horasDisponibles;
+    int ingredientesDisponibles;
+
+    
